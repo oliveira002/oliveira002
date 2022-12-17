cba74589adbdb51a283c5a9a6a9926f8991a8a4b
oliveira002 <deadzin112@gmail.com>
2022-12-17 19:45:13 +0000

    added small summary

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -7,8 +7,11 @@ import 'animate.css';
 export const Intro = () => {
   return (
     <div className ="banner">
-        <div className="typer h2 fw-bold">
+        <div className="typer h1 fw-bold">
             Hey! I'm Oliveira.
+        </div>
+        <div className='res h5 animate__animated animate__fadeIn animate__delay-2s'>
+            Welcome to my portfolio! I'm studying Informatics and Computing Engineering at FEUP. Here you can find the most interesting projects I developed throughout the course.
         </div>
     </div>
   )
